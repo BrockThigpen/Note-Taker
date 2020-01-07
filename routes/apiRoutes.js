289f13deb11064ba029f8b80b81c4bbeb76f7df0
Brock T <brockthigpen@yahoo.com>
2020-01-07 18:21:51 -0500

    fixed DELETE request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 const db = JSON.parse(fs.readFileSync('db/db.json'));
 // Routing
 module.exports = app => {
+    // Unique ID
+    let uniqueID = () => {
+        db.forEach((x, i) => {
+            x.id = i + 1;
+        });
+        return db;
+    }
     // GET requests
     app.get('/api/notes', (req, res) => {
         return res.json(uniqueID());
@@ -12,22 +19,14 @@ module.exports = app => {
         db.push(req.body);
         return res.json(uniqueID());
     });
-    // Unique ID
-    let uniqueID = () => {
-        db.forEach((x, i) => {
-            x.id = i + 1;
-        });
-        return db;
-    }
     // DELETE requests
     app.delete('/api/notes/:id', (req, res) => {
-        let input = req.body;
-        for (let i = 0; i < db.length; i++) {
-            if (db[i] === input) {
-                db.splice(i, 1);
-                i--;
-                return res.json(db);
-            }
-        }
+        const deleted = db.findIndex(i => i.id === req.params.id);
+        db.splice(deleted, 1);
+
+        return res.json(db);
+
     });
+
 }
+
