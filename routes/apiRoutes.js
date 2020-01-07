32e3645da3a9053255a7cafe6fb8d5f9b4718f63
Brock T <brockthigpen@yahoo.com>
2020-01-06 23:34:46 -0500

    improved ID system // improved POST requests // experimenting with DELETE requests

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,33 @@
 // Dependencies
 const fs = require('fs');
-const db = JSON.parse(fs.readFileSync('./db/db.json'));
+const db = JSON.parse(fs.readFileSync('db/db.json'));
 // Routing
 module.exports = app => {
     // GET requests
     app.get('/api/notes', (req, res) => {
-        return res.json(db);
+        return res.json(uniqueID());
     });
     // POST requests
     app.post('/api/notes', (req, res) => {
-        let newN = req.body;
-        console.log(newN);
+        db.push(req.body);
+        return res.json(uniqueID());
     });
     // Unique ID
-    db.forEach((x, i) => {
-        x.id = i+1;
-    });
+    let uniqueID = () => {
+        db.forEach((x, i) => {
+            x.id = i + 1;
+        });
+        return db;
+    }
     // DELETE requests
-    app.delete('/api/notes:id', (req,res) => {
-
+    app.delete('/api/notes/:id', (req, res) => {
+        let input = req.body;
+        for (let i = 0; i < db.length; i++) {
+            if (db[i] === input) {
+                db.splice(i, 1);
+                i--;
+                return res.json(db);
+            }
+        }
     });
-    console.log(db);
 }
