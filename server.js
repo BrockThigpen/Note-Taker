1e4528852430b0ac04b5c08bc18c83faa1d10b22
Brock T <brockthigpen@yahoo.com>
2020-01-06 20:14:23 -0500

    added routes folder // configured express server

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+const express = require('express');
+// Config
+const app = express();
+// Port
+const PORT = process.env.PORT || 3000;
+// Data parsing 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+// Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });  
