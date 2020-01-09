ae02355e7620fac1f3824128ae7d3b9d39386a85
Brock T <brockthigpen@yahoo.com>
2020-01-08 22:55:39 -0500

    added logic to rewrite bd after post and delete //

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,30 +3,34 @@ const fs = require('fs');
 const db = JSON.parse(fs.readFileSync('db/db.json'));
 // Routing
 module.exports = app => {
-    // Unique ID
+    // GET requests
+    app.get('/api/notes', (req, res) => {
+        return res.json(db);
+    });
+    // POST requests
+    app.post('/api/notes', (req, res) => {
+        let newN = req.body;
+        db.push(newN);
+        uniqueID();
+        reWrite();
+        res.json(db);
+    });
+    // DELETE requests
+    app.delete('/api/notes/:id', (req, res) => {
+        const deleted = db.findIndex((i) => i.id == req.params.id);
+        db.splice(deleted, 1);
+        reWrite();
+        res.json(db);
+    });
+    // Unique ID func
     let uniqueID = () => {
         db.forEach((x, i) => {
             x.id = i + 1;
         });
-        return db;
     }
-    // GET requests
-    app.get('/api/notes', (req, res) => {
-        return res.json(uniqueID());
-    });
-    // POST requests
-    app.post('/api/notes', (req, res) => {
-        db.push(req.body);
-        return res.json(uniqueID());
-    });
-    // DELETE requests
-    app.delete('/api/notes/:id', (req, res) => {
-        const deleted = db.findIndex(i => i.id === req.params.id);
-        db.splice(deleted, 1);
-
-        return res.json(db);
-
-    });
-
+    // Writefile func
+    let reWrite = () => {
+        let newDB = JSON.stringify(db);
+        fs.writeFile('db/db.json', newDB, err => { if (err) throw err });
+    }
 }
-
